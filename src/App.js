a5b273fbd7eaf9e570a85711aa63de34ef0af6e5
esseisan <scpvillegas@gmail.com>
2024-03-21 22:19:57 +0800

    adding files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,146 @@
-import logo from './logo.svg';
+import React, { useState, useEffect} from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import './App.css';
+import Home from './Home';
+import Login from './Login';
+import apiRequest from './apiRequest';
+
+
 
 function App() {
+
+// declare all const needed
+const [isLoggedIn, setIsLoggedIn] = useState(false);
+const [ipAddress, setIpAddress] = useState('');
+const [geoInfo, setGeoInfo] = useState(null);
+const [error, setError] = useState(null);
+const [email, setEmail] = useState('');
+const [password, setPassword] = useState('');
+
+
+const USER_CREDENTIALS = {
+  email: 'user@example.com',
+  password: 'password123',
+  ip: '136.158.126.94'
+};
+
+//link for Geo API
+const HOME_API_URL = 'https://ipinfo.io/';
+
+useEffect(() => {
+  const userLoggedIn = localStorage.getItem('isLoggedIn');
+  if (userLoggedIn === 'true') {
+    setIsLoggedIn(true);
+  }
+}, []);
+
+const handleLogin = (email, password) => {
+  if (email === USER_CREDENTIALS.email && password === USER_CREDENTIALS.password) {
+    setIsLoggedIn(true);
+    localStorage.setItem('isLoggedIn', 'true');
+    setIpAddress(USER_CREDENTIALS.ip);
+  } else {
+    alert('Invalid credentials. Please try again...');
+  }
+};
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  handleLogin(email, password);
+};
+
+//HOME FUNCTIONS =======================================================================================
+
+const handleLogout = () => {
+  setIsLoggedIn(false);
+  localStorage.removeItem('isLoggedIn');
+};
+
+const handleSearch = (search) => {
+  // Validate IP address format
+  const ipRegex = /^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$/;
+  if (ipRegex.test(ipAddress)) {
+    // Fetch geolocation data for the entered IP
+    fetch(`${HOME_API_URL}/${ipAddress}/geo`)
+      .then((response) => response.json())
+      .then((data) => {
+        setGeoInfo(data);
+        setError(null);
+      })
+      .catch((error) => {
+        console.error('Error fetching geolocation data:', error);
+        setError('Error fetching geolocation data. Please try again.');
+      });
+  } else {
+    setError('Invalid IP address format. Please enter a valid IP.');
+  }
+};
+
+const handleClear = () => {
+  // Reset the search, set IP Address to me: 136.158.126.94
+  setIpAddress('');
+  setError(null);
+  fetch(`${HOME_API_URL}/${USER_CREDENTIALS.ip}/geo`)
+    .then((response) => response.json())
+    .then((data) => setGeoInfo(data))
+    .catch((error) => console.error('Error fetching geolocation data:', error));
+};
+
+//HOME FUNCTIONS =======================================================================================
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  
+     
+        
+        <Routes>
+
+          <Route exact path='/login' element={isLoggedIn ? <Navigate to="/" /> : <Login 
+            onLogin={handleLogin}
+            email={email}
+            setEmail = {setEmail}
+            password = {password}
+            setPassword = {setPassword}  
+            handleSubmit={handleSubmit}
+          />}>
+            
+          </Route>
+          <Route exact path='/' element={ !isLoggedIn ? <Navigate to="/login" /> : 
+              <Home 
+                onLogout = {handleLogout}
+                geoInfo = {geoInfo}
+                setGeoInfo = {setGeoInfo}
+                HOME_API_URL={HOME_API_URL}
+                ipAddress = {ipAddress}
+                setIpAddress = {setIpAddress}
+                handleClear = {handleClear}
+                handleSearch = {handleSearch}
+                error = {error}
+              />}>  
+          </Route>
+
+        </Routes>
+      
+    
+
     </div>
   );
 }
 
 export default App;
+
+
+{/* <Route exact path='/login' element={<Login onLogin={handleLogin}/>}></Route>
+          <Route exact path='/' element={
+              <Home 
+                onLogout = {handleLogout}
+                geoInfo = {geoInfo}
+                setGeoInfo = {setGeoInfo}
+                HOME_API_URL={HOME_API_URL}
+                ipAddress = {ipAddress}
+                setIpAddress = {setIpAddress}
+                handleClear = {handleClear}
+                handleSearch = {handleSearch}
+                error = {error}
+              />}
+          /> */}
--- src/index.js
+++ src/index.js
@@ -3,14 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+//const mongoose = require('mongoose');
+
+
+//const dbOptions = {useNewUrlParser:true, useUnifiedTopology:true}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
-);
-
+); 
+//mongoose.connect('mongodb+srv://scpvillegas2:sGT8hFjEmyjuRrcH@cluster0.pxi8leq.mongodb.net/jlab_sample_db?retryWrites=true&w=majority&appName=Cluster0',dbOptions)
+//.then(() => console.log('DB Connected'))
+//.catch(err => console.log(err))
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
